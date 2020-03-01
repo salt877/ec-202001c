c3a2ac65678e8343e1726255b05f310f3729da48
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-03-01 15:25:37 +0900

    ログイン画面へ遷移させるコントローラーを作成しました。

---
+++ src/main/java/jp/co/example/ecommerce_c/controller/LoginController.java
@@ -0,0 +1,34 @@
+package jp.co.example.ecommerce_c.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * ログイン画面へ遷移させるコントローラー.
+ * 
+ * @author katsuya.fujishima
+ *
+ */
+@Controller
+public class LoginController {
+	
+	/**
+	 * ログイン画面へフォワードするメソッド.
+	 * 
+	 * @param model リクエストスコープ
+	 * @param error エラー
+	 * @return ログイン画面
+	 */
+	@RequestMapping("/showLogin")
+	public String showLogin(Model model, @RequestParam(required = false) String error) {
+		System.err.println("login error:" + error);
+		if (error != null) {
+			System.err.println("login failed");
+			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
+		}
+		return "login";
+	}
+
+}
