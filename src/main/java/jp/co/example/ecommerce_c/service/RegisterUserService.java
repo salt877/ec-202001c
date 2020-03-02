ab14412ea7abedb0e8808a3fa43b0516aab5c315
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-03-02 14:25:16 +0900

    ユーザ情報を操作するサービスクラス名を変更しました。

---
+++ src/main/java/jp/co/example/ecommerce_c/service/RegisterUserService.java
@@ -0,0 +1,40 @@
+package jp.co.example.ecommerce_c.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.example.ecommerce_c.domain.User;
+import jp.co.example.ecommerce_c.repository.UserRepository;
+
+/**
+ * ユーザ情報を操作するサービス.
+ * 
+ * @author rinashioda
+ *
+ */
+@Service
+@Transactional
+public class RegisterUserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	/**
+	 * ユーザ情報を登録します.
+	 * 
+	 * @param user ユーザ情報
+	 */
+	public void resister(User user) {
+		userRepository.insert(user);
+	}
+
+	/**
+	 * @param email メールアドレス
+	 * @return ユーザ情報 存在しない場合はnullを返します
+	 */
+	public User searchUserByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+}
