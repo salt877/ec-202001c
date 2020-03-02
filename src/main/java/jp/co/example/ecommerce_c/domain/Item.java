2fe6b5cd2de08582a23cedc723942f53f8b94c9b
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-03-02 14:37:21 +0900

    商品一覧を出力する機能を新規作成しました。

---
+++ src/main/java/jp/co/example/ecommerce_c/domain/Item.java
@@ -0,0 +1,94 @@
+package jp.co.example.ecommerce_c.domain;
+
+import java.util.List;
+
+public class Item {
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String description;
+	
+	private Integer priceM;
+	
+	private Integer priceL;
+	
+	private String imagePath;
+	
+	private Boolean deleted;
+
+	private List<topping> toppingList;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getPriceM() {
+		return priceM;
+	}
+
+	public void setPriceM(Integer priceM) {
+		this.priceM = priceM;
+	}
+
+	public Integer getPriceL() {
+		return priceL;
+	}
+
+	public void setPriceL(Integer priceL) {
+		this.priceL = priceL;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public Boolean getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public List<topping> getToppingList() {
+		return toppingList;
+	}
+
+	public void setToppingList(List<topping> toppingList) {
+		this.toppingList = toppingList;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", name=" + name + ", description=" + description + ", priceM=" + priceM + ", priceL="
+				+ priceL + ", imagePath=" + imagePath + ", deleted=" + deleted + "]";
+	}
+
+}
---
+++ src/main/java/jp/co/example/ecommerce_c/repository/ItemRepository.java
@@ -0,0 +1,63 @@
+package jp.co.example.ecommerce_c.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.example.ecommerce_c.domain.Item;
+
+@Repository
+public class ItemRepository {
+
+	/**
+	 * Itemオブジェクトを生成するローマッパー.
+	 */
+	private static final RowMapper<Item> ITEM_ROW_MAPPER = (rs,i) -> {
+		Item item = new Item();
+		item.setId(rs.getInt("id"));
+		item.setName(rs.getString("name"));
+		item.setDescription(rs.getString("description"));
+		item.setPriceM(rs.getInt("price_m"));
+		item.setPriceL(rs.getInt("price_l"));
+		item.setImagePath(rs.getString("image_path"));
+		item.setDeleted(rs.getBoolean("deleted"));
+		return item;
+	};
+		
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	/**
+	 * 商品一覧情報をID順で取得します.
+	 * 
+	 * @return 商品一覧
+	 */
+	public List<Item> findAll() {
+
+		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items ORDER BY id";
+
+		List<Item> itemList = template.query(sql, ITEM_ROW_MAPPER);
+
+		return itemList;
+	}
+
+	/**
+	 * 商品名から商品を(曖昧)検索します.
+	 * 
+	 * @param name 商品名
+	 * @return 検索された商品の一覧
+	 */
+	public List<Item> seachByItemName(String name) {
+		String sql = "SELECT id,name,description,price_m,price_l,image_path,deleted FROM items WHERE name LIKE :name ORDER BY id DESC";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
+
+		List<Item> itemList = template.query(sql, param, ITEM_ROW_MAPPER);
+		return itemList;
+	}
+
+}
