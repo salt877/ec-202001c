2b96a93404f00a9f680e83352f08333d54a3f51f
kthatake7 <kota.hatakeyama@rakus-partners.co.jp>
2020-03-03 00:12:13 +0900

    OrderRepositoryにupdateメソッドを追加(内容更新)

--- src/main/java/jp/co/example/ecommerce_c/repository/OrderRepository.java
+++ src/main/java/jp/co/example/ecommerce_c/repository/OrderRepository.java
@@ -6,15 +6,17 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
 
 import jp.co.example.ecommerce_c.domain.Order;
 
 /**
- * ordersテーブルを操作するリポジトリ
+ * ordersテーブルを操作するリポジトリ.
  * 
  * @author hatakeyamakouta
  *
  */
+@Repository
 public class OrderRepository {
 	
 	@Autowired
@@ -37,6 +39,13 @@ public class OrderRepository {
 		return order;
 	};
 	
+	/**
+	 * 引数のuserIdとstatusに一致した注文情報を取得する.
+	 * 
+	 * @param userId ユーザーid
+	 * @param status 注文状態(0.注文前 1.未入金 2.入金済 3.発送済 4.配送完了 9.キャンセル)
+	 * @return 引数のuserIdとstatusに一致した注文情報
+	 */
 	public Order findByUserIdAndStatus(Integer userId, Integer status) {
 		String sql = "SELECT id, user_id, total_price, order_date, destination_name, destination_email, destination_zipcode, "
 				+ "destination_address, destination_tel, delivery_time, payment_method "
@@ -44,7 +53,12 @@ public class OrderRepository {
 		SqlParameterSource param = new MapSqlParameterSource().addValue("user_id", userId).addValue("status", status);
 		return template.queryForObject(sql, param, ORDER_ROW_MAPPER);
 	}
-	
+		
+	/**
+	 * ordersテーブルに注文情報を挿入する.
+	 * 
+	 * @param order 注文情報
+	 */
 	public void insert(Order order) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
 		String insertSql = "INSERT INTO orders(user_id, status, total_price, order_date, destination_name, destination_email, destination_zipcode, "
@@ -52,5 +66,11 @@ public class OrderRepository {
 				+ "VALUES(:userId, :status, :totalPrice, :orderDate, :destinationName, :destinationEmail, :destinationZipcode, :destinationAddress, "
 				+ ":destinationTel, :deliveryTime, :paymentMethod;";
 		template.update(insertSql, param);
+	}
+	
+	public void update(Order order) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(order);
+		String updateSql = "UPDATE orders SET total_price = :totalPrice WHERE user_id = :userId AND status = :status;";
+		template.update(updateSql, param);
 	}
 }
