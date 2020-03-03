4001f01bddade5a1ca077ce01aee9c2127c7c42f
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-03-03 14:28:42 +0900

    カート内の商品のトッピングを削除するメソッドを作成しました。

--- src/main/java/jp/co/example/ecommerce_c/repository/OrderToppingRepository.java
+++ src/main/java/jp/co/example/ecommerce_c/repository/OrderToppingRepository.java
@@ -2,6 +2,7 @@ package jp.co.example.ecommerce_c.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
@@ -25,5 +26,16 @@ public class OrderToppingRepository {
 		String insertSql = "INSERT INTO order_toppings(topping_id, order_item_id) VALUES(:toppingId, :orderItemId);";
 		SqlParameterSource param = new BeanPropertySqlParameterSource(orderTopping);
 		template.update(insertSql, param);
+	}
+	
+	/**
+	 * orderItemIdが一致するトッピングを削除するメソッド.
+	 * 
+	 * @param orderItemId カート内の商品ID
+	 */
+	public void deleteItemById(Integer orderItemId) {
+		String sql = "DELETE FROM order_toppings WHERE order_item_id = :orderItemId";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("orderItemId", orderItemId);
+		template.update(sql, param);
 	}
 }
