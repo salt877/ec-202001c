1a52a83bf6fd290e2a0a934b31f814301f16e56d
kthatake7 <kota.hatakeyama@rakus-partners.co.jp>
2020-03-05 22:09:38 +0900

    支払方法で代金引換を入れた場合、クレジットカードフォームを隠すようにjsファイル作成

--- src/main/resources/static/js/creditcard_form.js
+++ src/main/resources/static/js/creditcard_form.js
@@ -1,9 +1,10 @@
-$(function() {
-	$("#payMethod").on("change", function() { // 選択肢変更時
-		if ($('input[name=paymentMethod]:checked').val() == "2") {
-			$("#creditForm").hide();
+$(function(){
+	$(".pay-method").on("change", function(){
+		var num = $("input[name='paymentMethod']:checked").val();
+		if(num == '2'){
+			$(".creditForm").hide();
 		}else{
-			$("#creditForm").show();
+			$(".creditForm").show();
 		}
 	});
 });
