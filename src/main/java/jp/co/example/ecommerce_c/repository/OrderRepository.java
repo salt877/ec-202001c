58466c642d5499d39055db8c2de87524f474be2f
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-03-08 18:38:03 +0900

    オーダー情報をサービスクラスで取得するためのfindByIdメソッドを作成しました。

--- src/main/java/jp/co/example/ecommerce_c/repository/OrderRepository.java
+++ src/main/java/jp/co/example/ecommerce_c/repository/OrderRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -169,22 +170,22 @@ public class OrderRepository {
 		return orderList;
 	};
 	
-//	private static final RowMapper<Order> ORDER_ROW_MAPPER = (rs, i) -> {
-//		Order order = new Order();
-//		order.setId(rs.getInt("id"));
-//		order.setUserId(rs.getInt("user_id"));
-//		order.setStatus(rs.getInt("status"));
-//		order.setTotalPrice(rs.getInt("total_price"));
-//		order.setOrderDate(rs.getDate("order_date"));
-//		order.setDestinationName(rs.getString("destination_name"));
-//		order.setDestinationEmail(rs.getString("destination_email"));
-//		order.setDestinationZipcode(rs.getString("destination_zipcode"));
-//		order.setDestinationAddress(rs.getString("destination_address"));
-//		order.setDestinationTel(rs.getString("destination_tel"));
-//		order.setDeliveryTime(rs.getTimestamp("delivery_time"));
-//		order.setPaymentMethod(rs.getInt("payment_method"));
-//		return order;
-//	};
+	private static final RowMapper<Order> ORDER_ROW_MAPPER = (rs, i) -> {
+		Order order = new Order();
+		order.setId(rs.getInt("id"));
+		order.setUserId(rs.getInt("user_id"));
+		order.setStatus(rs.getInt("status"));
+		order.setTotalPrice(rs.getInt("total_price"));
+		order.setOrderDate(rs.getDate("order_date"));
+		order.setDestinationName(rs.getString("destination_name"));
+		order.setDestinationEmail(rs.getString("destination_email"));
+		order.setDestinationZipcode(rs.getString("destination_zipcode"));
+		order.setDestinationAddress(rs.getString("destination_address"));
+		order.setDestinationTel(rs.getString("destination_tel"));
+		order.setDeliveryTime(rs.getTimestamp("delivery_time"));
+		order.setPaymentMethod(rs.getInt("payment_method"));
+		return order;
+	};
 
 //	/**
 //	 * 引数のuserIdとstatusに一致した注文情報を取得する.
@@ -299,5 +300,13 @@ public class OrderRepository {
 		String updateSql = "UPDATE orders SET total_price = total_price + :totalPrice WHERE id = :id";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("totalPrice", totalPrice).addValue("id", id);
 		template.update(updateSql, param);
+	}
+	
+	public Order findByOrderId(Integer id) {
+		String loadsql = "SELECT id, user_id, status, total_price, order_date, destination_name, destination_email, "
+				+ "destination_zipcode, destination_address, destination_tel, destination_tel, payment_method "
+				+ "FROM orders WHERE id = :id;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id); 
+		return template.queryForObject(loadsql, param, ORDER_ROW_MAPPER);
 	}
 }
