c8f27764307c6191448160a8a8a6a7ee7f45e2e1
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-03-09 16:55:35 +0900

    クレジット情報の認証APIを実装しました。要修正

--- src/main/resources/static/js/creditcard_form.js
+++ src/main/resources/static/js/creditcard_form.js
@@ -1,33 +1,47 @@
-$(function(){
-	$(".pay-method").on("change", function(){
+$(function() {
+	$(".pay-method").on("change", function() {
 		var num = $("input[name='paymentMethod']:checked").val();
-		if(num == '2'){
+		if (num == '2') {
 			$(".creditForm").hide();
-		}else{
+		} else {
 			$(".creditForm").show();
 		}
 	});
 	
-	$("#order-btn").on("click",function(){
-		$.ajax({
-			url:"http://153.126.174.131:8080/sample-credit-card-web-api/credit-card/payment", //送信先のWebAPIのURL
-			dataType:"jsonp", //レスポンスデータ形式今回は最後にpをつける
-			data:{  //リクエストパラメータ情報
-				zipcode:$('#zipcode').val()
-			},
-			async:true //非同期で処理を行う
-		}).done(function(data){
-			//検索成功時にはページに結果を反映
-			//コンソールに取得したJSONデータを表示
-			console.dir(JSON.stringify(data));
-			$("#address").val(data.items[0].address); //住所欄に住所をセット
-		}).fail(function(XMLHttpRequest, textStatus, errorThrown){
-			//検索失敗時には、その旨をダイアログ表示しエラー情報をコンソールに記載
-			alert('正しい結果を得られませんでした。');
-			console.log("XMLHttpRequest:" + XMLHttpRequest.status);
-			console.log("textStatus:" + textStatus);
-			console.log("errorThrown:" + errorThrown.message);
-		});
+	$("#bbb").on("click",function(){
+		console.log($("#user_id").val());
+		console.log($("#order_number").val());
+		console.log($("#amount").val());
+		console.log($("#card_number").val());
+		console.log($("#card_exp_year").val());
+		console.log($("#card_exp_month").val());
+		console.log($("#card_name").val());
+		console.log($("#card_cvv").val());
 	});
 	
+	$("#error2").hide().css("color","red").css("font-weight","bold");
+	
+	$("#order-btn").on("click",function(){
+		$.ajax({
+			url:"http://192.168.2.108:8080/sample-credit-card-web-api/credit-card/payment", //送信先のWebAPIのURL
+			dataType:"json", //レスポンスデータ形式
+			data:{  //リクエストパラメータ情報
+				user_id: $("#user_id").val(),
+				order_number: $("#order_number").val(),
+				amount: $("#amount").val(),
+				card_number: $("#card_number").val(),
+				card_exp_year: $("#card_exp_year").val(),
+				card_exp_month: $("#card_exp_month").val(),
+				card_name: $("#card_name").val(),
+				card_cvv: $("#card_cvv").val()
+			},
+			async:false //同期処理を行う
+		}).done(function(data){
+			if($("#card_status").val(data.items[0].status) != "success"){
+				$("#error2").show();
+			}
+			; 
+		});
+	});
+
 });
